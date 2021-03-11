ad4eddbedcb854f165c12c17cdfb4c5ef4a04dda
zhazgul004 <zhazgul004@gmail.com>
2021-03-11 12:20:27 +0600

    Added Swagger documentation & Updated database properties for remote

---
+++ src/main/java/kg/PerfectJob/BookStore/boot/Init.java
@@ -0,0 +1,21 @@
+package kg.PerfectJob.BookStore.boot;
+
+import kg.PerfectJob.BookStore.entity.Role;
+import kg.PerfectJob.BookStore.entity.User;
+import kg.PerfectJob.BookStore.repository.RoleRepository;
+import kg.PerfectJob.BookStore.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+
+public class Init implements CommandLineRunner {
+
+    @Autowired private UserRepository userRepository;
+    @Autowired private RoleRepository roleRepository;
+
+
+    @Override
+    public void run(String... args) throws Exception {
+//        Role role = roleRepository.save(new Role(0, "ROLE_ADMIN"));
+  //      userRepository.save(new User(0, "admin@gmail.com", "***REMOVED***", "Admin", null, true, null, role));
+    }
+}
