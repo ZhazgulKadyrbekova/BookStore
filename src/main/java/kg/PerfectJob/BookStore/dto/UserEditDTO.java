8e98ae3be4b2198dc12dfac6793f07904aaeeea2
zhazgul <zhazgul004@gmail.com>
2021-04-08 23:01:04 +0600

    Account of reader

---
+++ src/main/java/kg/PerfectJob/BookStore/dto/UserEditDTO.java
@@ -0,0 +1,14 @@
+package kg.PerfectJob.BookStore.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class UserEditDTO {
+    private String name;
+    private String surname;
+}
